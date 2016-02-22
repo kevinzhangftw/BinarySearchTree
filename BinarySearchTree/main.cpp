c889cf0f974b2ab29109e420161603bcb1135f0a
Kai Zhang <kwz@asb9700-e02.csil.sfu.ca>
2016-02-22 10:57:03 -0800

    Initial Commit

---
+++ BinarySearchTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinarySearchTree
+//
+//  Created by Kai Zhang on 2016-02-22.
+//  Copyright © 2016 Kai Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
